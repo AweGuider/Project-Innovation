f118e03bb2a147a09c17c8594ceca26e8bb1d298
hansjehansjehansje <hansjedispa@live.nl>
2023-04-12 12:44:25 +0200

    Fixed the train system and pivot point of cart1

--- PIGame/Assets/Scripts/Interactables/Train/Carriage1.cs
+++ PIGame/Assets/Scripts/Interactables/Train/Carriage1.cs
@@ -8,18 +8,20 @@ public class Carriage1 : MonoBehaviour
     public PathCreator pathCreator;
     public float speed = -5f;
     float distanceTravelled;
+    public float padding = 0f;
+    public float height = 1.5f;
 
     private void Start()
     {
         pathCreator = GameObject.FindGameObjectWithTag("Tracks").GetComponent<PathCreator>();
-
     }
     // Update is called once per frame
     void Update()
     {
         distanceTravelled += speed * Time.deltaTime;
-        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
-        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled + padding);
+        transform.position += new Vector3(0, height, 0);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled + padding);
         transform.rotation *= Quaternion.AngleAxis(90, Vector3.up);
     }
 }
