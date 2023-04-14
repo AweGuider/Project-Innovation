fdbe912dedc65f5b3f8927ecb8ea55ad6b06214f
AweGuider <aweguider@gmail.com>
2023-04-14 20:32:29 +0200

    Optimized cash register code

--- PIGame/Assets/Scripts/Interactables/CashRegister.cs
+++ PIGame/Assets/Scripts/Interactables/CashRegister.cs
@@ -7,27 +7,56 @@ using UnityEngine.UIElements;
 
 public class CashRegister : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed;
+    public Vector3 move;
     public Rigidbody rb;
     private bool isPressed = false;
+
+    [SerializeField]
+    private bool left;
+    [SerializeField]
+    private bool right;
+    [SerializeField]
+    private bool up;
+    [SerializeField]
+    private bool down;
     // Start is called before the first frame update
     void Start()
     {
-       rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        speed = 10f;
+        move = new();
+        if (left)
+        {
+            move.x = -1;
+        }
+        if (right)
+        {
+            move.x = 1;
+        }
+        if (down)
+        {
+            move.z = -1;
+        }
+        if (up)
+        {
+            move.z = 1;
+        }
+        move *= speed;
     }
 
     public void PushOut()
     {
         if (isPressed == false)
         {
-            rb.velocity = new Vector3(0, 0, -10);
-            isPressed = true;
+            rb.velocity = move;
         }
 
         else if (isPressed == true)
         {
-            rb.velocity = new Vector3(0, 0, 10);
-            isPressed = false;
+            rb.velocity = move * -1;
         }
+
+        isPressed = !isPressed;
     }
 }
