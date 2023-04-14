e52871471e61b6b3d821c86f67b954b840294a26
hansjehansjehansje <hansjedispa@live.nl>
2023-04-14 13:00:45 +0200

    New train

---
+++ PIGame/Assets/Scripts/Interactables/Train/Carriage2.cs
@@ -0,0 +1,27 @@
+using PathCreation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carriage2 : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = -10f;
+    float distanceTravelled;
+    public float padding = 0f;
+    public float height = 1.5f;
+
+    private void Start()
+    {
+        pathCreator = GameObject.FindGameObjectWithTag("Tracks").GetComponent<PathCreator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled + padding);
+        transform.position += new Vector3(0, height, 0);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled + padding);
+        transform.rotation *= Quaternion.AngleAxis(90, Vector3.up);
+    }
+}
