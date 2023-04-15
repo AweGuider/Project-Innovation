411da99c4f224fdf46233b1a75279596adfe1386
TheRealMrPepper <chivutudor@gmail.com>
2023-04-16 00:02:48 +0200

    Fence door & spotlight to chchken, some map changes

--- PIGame/Assets/Scripts/Interactables/DoorTrap.cs
+++ PIGame/Assets/Scripts/Interactables/DoorTrap.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class DoorTrap : Trap
 {
+    //private bool open = false;
+    [SerializeField]
+    private Animator anim;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Open_Door();
+            Debug.Log("F");
+        }
+    }
+
+    void Open_Door()
+    {
+        if(anim.GetBool("Open") == false)
+        {
+            anim.SetBool("Open", true);
+        }
+        else
+        {
+            anim.SetBool("Open", false);
+        }      
     }
 
     public override void ActivateTrap()
