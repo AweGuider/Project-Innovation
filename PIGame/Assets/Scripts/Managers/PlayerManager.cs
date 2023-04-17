bd56a7deeb2ae8205e5730a792b3007861b6ac55
TheRealMrPepper <chivutudor@gmail.com>
2023-04-17 18:55:33 +0200

    Last prefabs + Mats + Level changes + Ratio

--- PIGame/Assets/Scripts/Managers/PlayerManager.cs
+++ PIGame/Assets/Scripts/Managers/PlayerManager.cs
@@ -38,7 +38,7 @@ public class PlayerManager : MonoBehaviourPunCallbacks
             }
             try
             {
-                //photonView.RequestOwnership();
+                photonView.RequestOwnership();
 
                 photonView.RPC("SpawnPlayer", RpcTarget.MasterClient, PhotonNetwork.LocalPlayer, _role, _team);
             }
@@ -60,7 +60,6 @@ public class PlayerManager : MonoBehaviourPunCallbacks
                 Debug.LogError($"Couldn't find Trap Controller: {e.Message}");
             }
         }
-        photonView.RequestOwnership();
     }
 
     // Update is called once per frame
